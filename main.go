23cd7c20264c8578941092ff7b16db94f6195824
Benoit Georgelin <benoit.georgelin@web4all.fr>
2018-02-08 18:29:45 -0500

    feat(main): initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+        "github.com/yulPa/yulparserApache"
+        "flag"
+        "fmt"
+        "log"
+)
+
+func main() {
+
+        log_path := flag.String("log", "", "Log Path")
+        flag.Parse()
+
+        stats, err := yulparserApache.ParseLogFile_Get_Stats(*log_path)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        // print JSON :)
+        fmt.Println(string(stats))
+}
